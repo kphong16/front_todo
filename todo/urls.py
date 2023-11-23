28907e9c1fc62e9eb5df9aa4e38cb607dee4a143
KP_Hong <kphong16@daum.net>
2023-11-23 19:48:28 +0900

    Add completed todos page

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('todos', views.todo_list, name='todo_list'),
     path('todo/<int:pk>', views.todo_detail, name='todo_detail'),
+    path('add_todo', views.add_todo, name='add_todo'),
+    path('completed_todos', views.completed_todos, name='completed_todos'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,24 @@
 # todo/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import requests
 
 # Create your views here.
 def todo_list(request):
     return render(request, 'todo/todo_list.html')
 
+def completed_todos(request):
+    return render(request, 'todo/completed_todos.html')
+
 def todo_detail(request, pk):
     return render(request, 'todo/todo_detail.html')
 
+def add_todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        response = requests.post('http://127.0.0.1:8000/todo/', json={'title':title, 'description':description})
+        if response.status_code == 201:
+            return redirect(reverse('todo_list'))
+    return render(request, 'todo/add_todo.html')
